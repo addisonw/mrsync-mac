925182bfc319d35b294ba0d943e4c582974b64f3
Apple <apple@gb-floater-n09.local>
2013-09-28 13:34:43 -0500

    reorganizing project

---
+++ mrsync-mac/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  mrsync-mac
+//
+//  Created by Apple on 9/28/13.
+//  Copyright (c) 2013 Addison P White. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
